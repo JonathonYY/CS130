38d8e346f6e4a56e4c6441c3d36fd8953df47eef
Michael Zhou <micha.zhou@gmail.com>
2025-02-19 10:25:44 -0800

    patch listing implemented

--- src/lib/firebase/firestore/types.ts
+++ src/lib/firebase/firestore/types.ts
@@ -1,5 +1,16 @@
 import { Timestamp } from "firebase/firestore";
 
+export interface PatchListingData {
+  title: string,
+  price: number,
+  condition: string,
+  category: string,
+  description: string,
+  selected_buyer: string, // buyer user_id
+  potential_buyers: string[], // user_ids of potential buyers
+  image_paths: string[], // list of paths to imgs
+}
+
 export interface Listing {
   updated: Timestamp,
   title: string,
