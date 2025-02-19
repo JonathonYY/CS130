208908a85da501996ff1bc0eded5fb9ea905e027
JonathonYY <43181115+JonathonYY@users.noreply.github.com>
2025-02-19 09:13:47 -0800

    initial report implementation

--- src/app/api/listing/[listing_id]/report/route.ts
+++ src/app/api/listing/[listing_id]/report/route.ts
@@ -1,4 +1,7 @@
 import { NextResponse } from "next/server";
+import {db} from "@/lib/firebase/config";
+import {getDoc, doc, updateDoc, arrayUnion} from "firebase/firestore";
+import { Listing } from "@/lib/firebase/firestore/types";
 
 /*
  * Report a Listing by id
@@ -15,13 +18,39 @@ export async function PATCH(
   req: Request,
   { params }: { params: Promise<{ listing_id: string }> }
 ) {
-  // get URL parameter listing_id
-  const listing_id = (await params).listing_id;
+  try {
+    // get URL parameter listing_id
+    const listing_id = (await params).listing_id;
 
-  // get reporter data from req body
-  const data = await req.json();
+    // get reporter data from req body
+    const {user_id} = await req.json();
 
-  // TODO: update listing in db
+    const listingRef = doc(db, 'listings', listing_id);
+    const listingSnapshot = await getDoc(listingRef);
 
-  return NextResponse.json({ data: { listing_id: listing_id }, error: null });
+    if (!listingSnapshot.exists()) {
+      throw new Error("Listing not found");
+    }
+
+    const listingData = listingSnapshot.data() as Listing;
+    if (!listingData) {
+      throw new Error("Listing data invalid");
+    }
+
+    const {reporters} = listingData;
+
+    if (reporters.includes(user_id)) {
+      throw new Error("User already reported listing");
+    }
+
+    await updateDoc(listingRef, {reporters: arrayUnion(user_id)});
+
+    return NextResponse.json({ data: { listing_id: listing_id }, error: null });
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return NextResponse.json({ data: null, error: e.message });
+    } else {
+      return NextResponse.json({ data: null, error: "unknown error" });
+    }
+  }
 }
