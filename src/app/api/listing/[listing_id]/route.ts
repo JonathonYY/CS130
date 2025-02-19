0f543f0e1b0d843c44e3a69c40b33638fc11ae08
JonathonYY <43181115+JonathonYY@users.noreply.github.com>
2025-02-19 10:54:40 -0800

    Implement listing delete API

--- src/app/api/listing/[listing_id]/route.ts
+++ src/app/api/listing/[listing_id]/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
-import { newListing } from "@/lib/firebase/firestore/types";
+import {db} from "@/lib/firebase/config";
+import { User, Listing, newListing } from "@/lib/firebase/firestore/types";
+import { getDoc, doc, updateDoc, arrayRemove, deleteDoc } from "firebase/firestore";
 
 /*
  * Get a Listing by id
@@ -63,7 +65,7 @@ export async function PATCH(
  * Params:
  *  listing_id: id of the Listing to get
  * Request body:
- *  None
+ *  user_id: id of the User deleting listing
  * Return:
  *  data: id of the deleted Listing
  *  error: error or null
@@ -72,10 +74,69 @@ export async function DELETE(
   req: Request,
   { params }: { params: Promise<{ listing_id: string }> }
 ) {
-  // get URL parameter listing_id
-  const listing_id = (await params).listing_id;
+  try {
+    // get URL parameter listing_id
+    const listing_id = (await params).listing_id;
 
-  // TODO: delete listing in db
+    // get user id from req body
+    const {user_id} = await req.json();
 
-  return NextResponse.json({ data: { listing_id: listing_id }, error: null });
+    const listingRef = doc(db, 'listings', listing_id);
+    const listingSnapshot = await getDoc(listingRef);
+
+    if (!listingSnapshot.exists()) {
+      throw new Error("Listing not found");
+    }
+
+    const listingData = listingSnapshot.data() as Listing;
+    if (!listingData) {
+      throw new Error("Listing data invalid");
+    }
+
+    // extract relevant fields
+    const {owner, potential_buyers} = listingData;
+
+    if (user_id === owner) {
+      const ownerRef = doc(db, 'users', owner);
+      const ownerSnapshot = await getDoc(ownerRef);
+      if (!ownerSnapshot.exists()) {
+        // do not throw an error from API call, but log problem
+        console.error(`owner ${owner} not found`);
+      }
+      const ownerData = ownerSnapshot.data() as User;
+      if (ownerData) {
+        await updateDoc(ownerRef, { active_listings: arrayRemove(listing_id) });
+      } else {
+        // do not throw an error from API call, but log problem
+        console.error(`owner ${owner} data invalid`);
+      }
+      await Promise.all(potential_buyers.map(async (buyer) => {
+        const buyerRef = doc(db, 'users', buyer);
+        const buyerSnapshot = await getDoc(buyerRef);
+        if (!buyerSnapshot.exists()) {
+          // do not throw an error from API call, but log problem
+          console.error(`potential buyer ${buyer} not found`);
+        }
+        const buyerData = buyerSnapshot.data() as User;
+        if (buyerData) {
+          await updateDoc(buyerRef, { interested_listings: arrayRemove(listing_id) });
+        } else {
+          // do not throw an error from API call, but log problem
+          console.error(`owner ${owner} data invalid`);
+        }
+      }));
+    } else {
+      throw new Error("Unauthorized user");
+    }
+
+    await deleteDoc(listingRef);
+
+    return NextResponse.json({ data: { listing_id: listing_id }, error: null });
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return NextResponse.json({ data: null, error: e.message });
+    } else {
+      return NextResponse.json({ data: null, error: "unknown error" });
+    }
+  }
 }
