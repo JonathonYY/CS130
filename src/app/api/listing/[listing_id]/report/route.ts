89af62ab884d06e0c8739821acf9bfb38680423d
JonathonYY <43181115+JonathonYY@users.noreply.github.com>
2025-02-23 20:47:00 -0800

    Update report API
    Only allow valid users to report a listing
    Enforce timeout between reports
    Automatically delete listing once it has been reported a
    certain number of times

--- src/app/api/listing/[listing_id]/report/route.ts
+++ src/app/api/listing/[listing_id]/report/route.ts
@@ -1,7 +1,11 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/firebase/config";
-import { getDoc, doc, updateDoc, arrayUnion, serverTimestamp } from "firebase/firestore";
-import { Listing } from "@/lib/firebase/firestore/types";
+import { getDoc, doc, updateDoc, arrayUnion, serverTimestamp, Timestamp } from "firebase/firestore";
+import { Listing, User } from "@/lib/firebase/firestore/types";
+import deleteListing from "@/lib/firebase/firestore/listing/deleteListing";
+
+const time_threshold = 60000; // number of milliseconds required since last report attempt
+const removal_threshold = 5; // number of reports required to autodelete listing
 
 /*
  * Report a Listing by id
@@ -37,13 +41,37 @@ export async function PATCH(
       throw new Error("Listing data invalid");
     }
 
-    const {reporters} = listingData;
+    const {reporters, owner} = listingData;
 
     if (reporters.includes(user_id)) {
       throw new Error("User already reported listing");
     }
 
-    await updateDoc(listingRef, { reporters: arrayUnion(user_id), updated: serverTimestamp() });
+    const reporterRef = doc(db, 'users', user_id);
+    const reporterSnapshot = await getDoc(reporterRef);
+
+    if (!reporterSnapshot.exists()) {
+      throw new Error("User not found");
+    }
+
+    const reporterData = reporterSnapshot.data() as User;
+    if (!reporterData) {
+      throw new Error("User data invalid");
+    }
+
+    const {last_reported} = reporterData;
+
+    const new_timestamp = Timestamp.now();
+    if (new_timestamp.toMillis() - last_reported.toMillis() < time_threshold) {
+      throw new Error("User must wait to report another listing");
+    }
+    await updateDoc(reporterRef, {last_reported: new_timestamp});
+
+    if (reporters.length + 1 >= removal_threshold) {
+      deleteListing(listing_id, owner);
+    } else {
+      await updateDoc(listingRef, { reporters: arrayUnion(user_id), updated: serverTimestamp() });
+    }
 
     return NextResponse.json({ data: { listing_id: listing_id }, error: null });
   } catch (e: unknown) {
