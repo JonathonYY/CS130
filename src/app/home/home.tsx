32c809086fe57f9a11bd6e9895d418792d10bd66
KevZ1209 <kevinbowditch11@gmail.com>
2025-02-26 13:34:27 -0800

    add user details fetching for front page

--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -1,39 +1,51 @@
-'use client'
+"use client";
 
 import HomeGrid from "../../components/homeGrid";
 import { useRouter } from "next/navigation";
 import "../globals.css";
-
+import { auth } from "@/lib/firebase/config";
+import { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 
 const Home: React.FC = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <div>
-            <div className="logoContainer">
-                <img 
-                    src="logo1.png" 
-                    alt="home page logo" 
-                    className="logoGeneral logoHome"
-                    onClick={() => router.push("/")}
-                />
+  const [currUser, setCurrUser] = useState(null);
 
-                <p>Search bar goes here</p>
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setCurrUser(user);
+    } else {
+      setCurrUser(null);
+    }
+  });
 
-                <img 
-                    src="icon.png"
-                    alt="user icon"
-                    className="userIcon"
-                    onClick={() => router.push("/login")}
-                />
-            </div>
+  return (
+    <div>
+      <h1>Hello {currUser?.displayName}</h1>
+      <div className="logoContainer">
+        <img
+          src="logo1.png"
+          alt="home page logo"
+          className="logoGeneral logoHome"
+          onClick={() => router.push("/")}
+        />
 
-            <hr />
+        <p>Search bar goes here</p>
 
-            <HomeGrid />
-        </div>
-    );
-}
+        <img
+          src={currUser?.photoURL ?? "icon.png"}
+          alt="user icon"
+          className="userIcon"
+          onClick={() => router.push("/login")}
+        />
+      </div>
 
+      <hr />
+
+      <HomeGrid />
+    </div>
+  );
+};
 
 export default Home;
