1a80b0be44b086850869cad87415226fce5db1f8
Michael Zhou <micha.zhou@gmail.com>
2025-02-27 11:18:42 -0800

    bugfixes

--- src/lib/firebase/firestore/listing/getListing.ts
+++ src/lib/firebase/firestore/listing/getListing.ts
@@ -1,6 +1,5 @@
 import { db } from "../../config";
 import { doc, getDoc } from "firebase/firestore";
-import { GetListingOutput } from "../types";
 
 export default async function getListing(doc_id: string) {
     const docRef = doc(db, "listings", doc_id);
