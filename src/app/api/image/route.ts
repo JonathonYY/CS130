d930f20fb070311cd839b91665c20844aedfa4b2
Eric Liu <eliu0120@gmail.com>
2025-02-28 15:15:59 -0800

    adding in image api files from kevin's branch

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -1,11 +1,27 @@
-import { NextResponse } from "next/server";
+// api/image/route.ts
+import { NextRequest, NextResponse } from "next/server";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { storage } from "@/lib/firebase/config";
 
-// TODO: INPUT/OUTPUT SHAPE ARE NOT SET
+export async function POST(request: NextRequest) {
+  try {
+    const formData = await request.formData();
+    const image = formData.get("image") as File;
 
-export async function POST(req: Request) {
-  return NextResponse.json({ data: { path: "" }, error: null });
-}
+    if (!image) {
+      return NextResponse.json({ data: null, error: "No image provided" });
+    }
 
-export async function GET(req: Request) {
-  return NextResponse.json({ data: { img: "" }, error: null });
+    const bytes = await image.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    const storageRef = ref(storage, `images/${image.name}`);
+    await uploadBytes(storageRef, buffer); // Use the Buffer
+    const url = await getDownloadURL(storageRef);
+
+    return NextResponse.json({ data: url, error: null });
+  } catch (error) {
+    console.error("Error uploading image to Firebase:", error);
+    return NextResponse.json({ data: null, error: "Internal server error" });
+  }
 }
