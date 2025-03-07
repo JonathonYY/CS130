09bbe90384e3db9ab05d3b510193032f1dbf0280
davaron <djiaronglin@gmail.com>
2025-03-07 15:06:11 -0800

    Remove unused Link import from Modify Listing page

--- src/app/modify-listing/page.tsx
+++ src/app/modify-listing/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import "../globals.css";
 import UpdateListingForm from "@/components/ItemUpdateForm"
