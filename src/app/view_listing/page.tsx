51bb680ec014f208f05db6fc022bd82a69d5d9b5
davaron <djiaronglin@gmail.com>
2025-03-08 03:21:55 -0800

    Remove rendering of Report Button when listing owner is current user

--- src/app/view_listing/page.tsx
+++ src/app/view_listing/page.tsx
@@ -6,6 +6,7 @@ import ReportButton from "@/components/ReportButton"
 
 import { useRouter, useSearchParams } from "next/navigation";
 import React, { useState, useEffect } from "react";
+import {useAuth} from "@/lib/authContext";
 
 interface ListingObject {
   id: string;
@@ -38,6 +39,7 @@ function getDateFromTimestamp(secs: number, nanos: number): string {
 }
 
 const Listing: React.FC = () => {
+  const {user} = useAuth();
   const searchParams = useSearchParams();
   const id = searchParams.get("id"); // use this id to call backend function to get full item details
   const router = useRouter();
@@ -65,6 +67,10 @@ const Listing: React.FC = () => {
     return <p>Loading...</p>;
   }
   
+  if (!user) {
+    return <p>No user found</p>;
+  }
+
   if (!listing) {
     return <p>No listing found</p>;
   }
@@ -88,7 +94,8 @@ const Listing: React.FC = () => {
         <div className="viewListingsTitle">
           <PriceTag price={listing.price}></PriceTag>
           {listing.title}
-          <ReportButton idObj={id}/>
+          {listing.owner != user.uid && <ReportButton idObj={id}/>}
+          
         </div>
         
         <Slideshow images={displayImages} timestamp={dateString} listingObj={listing}></Slideshow>
