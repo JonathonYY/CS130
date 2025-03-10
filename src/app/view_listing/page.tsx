8bb2b5c474f2cfd2f6e88ae5790579f643226612
KevZ1209 <kevinbowditch11@gmail.com>
2025-03-09 17:52:06 -0700

    add new express interest button

--- src/app/view_listing/page.tsx
+++ src/app/view_listing/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import Slideshow from "@/components/ItemPictureDeck";
-import PriceTag from "@/components/PriceTag"
-import ReportButton from "@/components/ReportButton"
+import PriceTag from "@/components/PriceTag";
+import ReportButton from "@/components/ReportButton";
 
 import { useRouter, useSearchParams } from "next/navigation";
 import React, { useState, useEffect } from "react";
@@ -19,7 +19,7 @@ interface ListingObject {
   owner: string;
   owner_name: string;
   owner_pfp: string;
-  potential_buyers: string[]; 
+  potential_buyers: string[];
   selected_buyer: string;
   seller_rating: number;
   updated: Timestamp;
@@ -33,9 +33,17 @@ interface Timestamp {
 function getDateFromTimestamp(secs: number, nanos: number): string {
   const ms = secs * 1000 + nanos / 1e6;
   const date = new Date(ms);
-  const formatTime = date.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit", second: "2-digit" });
-  const formatDate = date.toLocaleDateString("en-US", { year: "numeric", month: "short", day: "numeric" });
-  return `${formatDate} at ${formatTime}`
+  const formatTime = date.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  const formatDate = date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+  return `${formatDate} at ${formatTime}`;
 }
 
 const Listing: React.FC = () => {
@@ -43,7 +51,7 @@ const Listing: React.FC = () => {
   const searchParams = useSearchParams();
   const id = searchParams.get("id"); // use this id to call backend function to get full item details
   const router = useRouter();
-  const [listing, setListing] = useState<ListingObject | null>(null);;
+  const [listing, setListing] = useState<ListingObject | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -55,7 +63,7 @@ const Listing: React.FC = () => {
   }, [user, router]);
 
   useEffect(() => {
-    async function fetchListingById(listingId : string | null) {
+    async function fetchListingById(listingId: string | null) {
       if (!token) {
         console.log("Unauthorized user");
         setLoading(false);
@@ -64,7 +72,7 @@ const Listing: React.FC = () => {
       }
       const response = await fetch(`/api/listing/${listingId}`, {
         method: "GET",
-        headers: { "Authorization": `Bearer ${token}`, },
+        headers: { Authorization: `Bearer ${token}` },
       });
       const { data, error } = await response.json();
       if (error) {
@@ -77,7 +85,7 @@ const Listing: React.FC = () => {
       }
     }
     fetchListingById(id);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id, user]);
 
   if (loading) {
@@ -94,10 +102,11 @@ const Listing: React.FC = () => {
   const timestampSec = listing.updated.seconds;
   const timestampNano = listing.updated.nanoseconds;
   const dateString = getDateFromTimestamp(timestampSec, timestampNano);
-  const displayImages = listing.image_paths.length === 0 ? ["noimage.png"] : listing.image_paths;
+  const displayImages =
+    listing.image_paths.length === 0 ? ["noimage.png"] : listing.image_paths;
   return (
     <div>
-      <div style={{ float: "right", padding: "10px 0px"}}>
+      <div style={{ float: "right", padding: "10px 0px" }}>
         <img
           src="logo1.png"
           alt="logo"
@@ -110,11 +119,14 @@ const Listing: React.FC = () => {
         <div className="viewListingsTitle">
           <PriceTag price={listing.price}></PriceTag>
           {listing.title}
-          {listing.owner != user.uid && <ReportButton idObj={id}/>}
+          {listing.owner != user.uid && <ReportButton idObj={id} />}
         </div>
 
-        <Slideshow images={displayImages} timestamp={dateString} listingObj={listing}></Slideshow>
-
+        <Slideshow
+          images={displayImages}
+          timestamp={dateString}
+          listingObj={listing}
+        ></Slideshow>
       </div>
     </div>
   );
